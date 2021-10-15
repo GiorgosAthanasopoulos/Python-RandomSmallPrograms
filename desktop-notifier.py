becc4e8800a373173016b17decaba2c8a4b2fbdd
Giorgos Athanassopoulos <90207655+giorgos-athanassopoulos@users.noreply.github.com>
2021-10-15 18:43:45 +0300

    Add files via upload

---
+++ desktop-notifier.py
@@ -0,0 +1,10 @@
+from plyer import notification
+
+notis = notification.notify(
+   title='NOTIFICATION',
+   message='HELLO IM UNDER THE WATER PLEASE HELP ME',
+   app_name='VSC-PYTHON',
+   timeout=10,
+   ticker='omegalol',
+   toast=False
+)
